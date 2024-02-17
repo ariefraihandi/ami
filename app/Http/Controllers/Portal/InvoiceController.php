d4b8979f2a6f065e95ad290e42a824543f655522
ariefraihandi <raihandi93@gmail.com>
2024-02-18 03:58:18 +0700

    aw

--- app/Http/Controllers/Portal/InvoiceController.php
+++ app/Http/Controllers/Portal/InvoiceController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Portal;
-
+ 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -101,7 +101,12 @@ class InvoiceController extends Controller
         $totalAmountYest        = Invoice::with('customer')->whereDate('created_at', '<=', Carbon::yesterday())->sum('total_amount');
         $invoicesDueToday       = Invoice::with('customer')->whereDate('due_date', '<=', Carbon::today())->where('status', '!=', 2)->where(function ($query) {$query->where('total_amount', '>', 0)->orWhere('panjar_amount', '>', 0);})->get();
         $invoicesDueYesterday   = Invoice::with('customer')->whereDate('due_date', '<=', Carbon::yesterday())->where('status', '!=', 2)->where(function ($query) {$query->where('total_amount', '>', 0)->orWhere('panjar_amount', '>', 0);})->get();
-        $totalUnAmount          = Invoice::with('customer')->whereDate('created_at', '<=', Carbon::today())->where('status', '!=', 2)->selectRaw('SUM(total_amount - panjar_amount) as total_unamount')->first()->total_unamount;
+        $totalUnAmount = Invoice::with('customer')
+    ->whereDate('created_at', '<=', Carbon::today())
+    ->whereRaw('(total_amount > panjar_amount)') // Memeriksa apakah total_amount lebih besar dari panjar_amount
+    ->selectRaw('SUM(total_amount - panjar_amount) as total_unamount')
+    ->first()->total_unamount;
+
         $totalUnAmountYest      = Invoice::with('customer')->whereDate('created_at', '<=', Carbon::yesterday())->where('status', '!=', 2)->selectRaw('SUM(total_amount - panjar_amount) as total_unamount')->first()->total_unamount;
 
 
